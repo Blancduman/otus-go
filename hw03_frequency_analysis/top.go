7f3981c5be18d5ead042028ea49eda6fcfd450fd
Ibragim Abbasov <ibragim.abbasov@vitech.team>
2023-06-19 23:22:29 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,57 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type pairs []pair
+
+type pair struct {
+	amount int
+	word   string
+}
+
+func (p pairs) toWordsSlice() []string {
+	words := make([]string, len(p))
+
+	for i, v := range p {
+		words[i] = v.word
+	}
+
+	return words
+}
+
+func Top10(text string) []string {
+	wordToAmountMap := map[string]int{}
+
+	words := strings.Fields(text)
+	for _, word := range words {
+		wordToAmountMap[word]++
+	}
+
+	wordToAmountSlice := make(pairs, 0)
+
+	for k, v := range wordToAmountMap {
+		if v != 0 {
+			wordToAmountSlice = append(wordToAmountSlice, pair{
+				amount: v,
+				word:   k,
+			})
+		}
+	}
+
+	sort.Slice(wordToAmountSlice, func(i, j int) bool {
+		if wordToAmountSlice[i].amount == wordToAmountSlice[j].amount {
+			return strings.Compare(wordToAmountSlice[i].word, wordToAmountSlice[j].word) != 1
+		}
+
+		return wordToAmountSlice[i].amount > wordToAmountSlice[j].amount
+	})
+
+	if len(wordToAmountSlice) > 10 {
+		return wordToAmountSlice[:10].toWordsSlice()
+	}
+
+	return wordToAmountSlice.toWordsSlice()
 }
