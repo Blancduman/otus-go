59a83abbd1d92527469bc8fac0b5fc1db0b4b887
Blancduman <blancduman@gmail.com>
2023-09-10 18:57:23 +0300

    feat: done hw

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 var (
@@ -10,6 +15,73 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	readFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	defer func() {
+		err := readFile.Close()
+		if err != nil {
+			fmt.Printf("can not close read file file: %s", err)
+		}
+	}()
+
+	writeFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("can not close created file: %w", err)
+	}
+
+	defer func() {
+		err := writeFile.Close()
+		if err != nil {
+			fmt.Printf("can not close read file file: %s", err)
+		}
+	}()
+
+	stats, err := readFile.Stat()
+	if err != nil {
+		return fmt.Errorf("can not read file stats: %w", err)
+	}
+
+	readFileSize := stats.Size()
+
+	if offset > readFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	var readFileLimit int64
+
+	if limit == 0 || offset+limit > readFileSize {
+		readFileLimit = readFileSize - offset
+	} else {
+		readFileLimit = limit
+	}
+
+	_, err = readFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("can not seek read file: %w", err)
+	}
+
+	pBar := progressbar.Default(readFileLimit, "copying")
+	defer func() {
+		err := pBar.Close()
+		if err != nil {
+			fmt.Printf("can not close progressbar: %s", err)
+		}
+	}()
+
+	writeFileProgress := io.MultiWriter(pBar, writeFile)
+
+	_, err = io.CopyN(writeFileProgress, readFile, readFileLimit)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("can not copy file: %w", err)
+	}
+
+	err = pBar.Finish()
+	if err != nil {
+		return fmt.Errorf("can not finish progressbar: %w", err)
+	}
+
 	return nil
 }
