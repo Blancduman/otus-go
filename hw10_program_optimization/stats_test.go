0b40a30c5323684674a8f0cb7dc07b371b057c1c
Blancduman <blancduman@gmail.com>
2023-09-24 22:10:02 +0300

    feat: tests

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,3 +1,4 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
@@ -33,6 +34,18 @@ func TestGetDomainStat(t *testing.T) {
 
 	t.Run("find 'unknown'", func(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("find 'COM'", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "COM")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{"browsecat.com": 2, "linktype.com": 1}, result)
+	})
+
+	t.Run("find empty", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(data), "")
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
 	})
