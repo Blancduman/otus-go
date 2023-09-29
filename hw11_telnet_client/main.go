14293ebfc0a8ede8587a6b7cfa601ff514e87ff8
Blancduman <blancduman@gmail.com>
2023-09-29 22:13:48 +0300

    hw done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,73 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var (
+	port    int
+	host    string
+	timeout time.Duration
+)
+
+func init() {
+	flag.IntVar(&port, "port", 3000, "port")
+	flag.StringVar(&host, "host", "localhost", "dns or ip address")
+	timeout = *flag.Duration("timeout", 10*time.Second, "timeout")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer cancel()
+
+	go func() {
+		s := make(chan os.Signal, 1)
+		signal.Notify(s, os.Interrupt)
+
+		<-s
+		cancel()
+	}()
+
+	flag.Parse()
+	address := fmt.Sprintf("%s:%d", host, port)
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatalf("fail to connect: %v", err)
+	}
+
+	defer func() {
+		err := client.Close()
+		if err != nil {
+			log.Fatalf("fail to close: %v", err)
+		}
+	}()
+
+	go func() {
+		err := client.Receive()
+		if err != nil {
+			log.Printf("fail to receive: %v", err)
+		}
+
+		cancel()
+	}()
+
+	go func() {
+		err := client.Send()
+		if err != nil {
+			log.Printf("fail to send: %v", err)
+		}
+
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
